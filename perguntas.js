9ed05b848074f7e9a2a5b7a945827f37729195d1
Tomeitol <142512610+Tomeitol@users.noreply.github.com>
2025-06-27 20:09:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é a moeda oficial dos Estados Unidos?',
+    'Dólar'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é o planeta mais próximo do Sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é a cidade mais populosa do mundo?',
+    'Tóquio'
+)
+
+criaCartao(
+    'Conhecimento Gerais',
+    'Qual é o país conhecido como a "terra do sol nascente"?',
+    'Japão'
+)
